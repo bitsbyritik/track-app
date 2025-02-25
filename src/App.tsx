b8ca99999ce0d135110ce8747661db2b5dc86298
bitsbyritik <bitsbyritik@gmail.com>
2025-02-25 19:03:30 +0530

    added-start-stop-trcaking

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [taskName, setTaskName] = useState("");
+  const [isTracking, setIsTracking] = useState<boolean>(false);
 
   async function startTracking() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -14,8 +15,18 @@ function App() {
     try {
       await invoke("set_task_name", { task: taskName });
       await invoke("start_tracking");
+      setIsTracking(true);
     } catch (err) {
       console.error("Error setting up task name: ", err);
+    }
+  }
+
+  async function stopTracking() {
+    try {
+      await invoke("stop_tracking");
+      setIsTracking(false);
+    } catch (err) {
+      console.error("Error during stop:", err);
     }
   }
 
@@ -23,14 +34,23 @@ function App() {
     <main className="container">
       <h1>Welcome to Track Event</h1>
 
-      <form className="row" onClick={startTracking}>
-        <input
-          id="greet-input"
-          onChange={(e) => setTaskName(e.currentTarget.value)}
-          placeholder="Enter a task name..."
-        />
-        <button type="submit">Start Tracking</button>
-      </form>
+      <div className="row">
+        {!isTracking ? (
+          <div>
+            <input
+              id="greet-input"
+              onChange={(e) => setTaskName(e.currentTarget.value)}
+              placeholder="Enter a task name..."
+            />
+            <button onClick={startTracking}>Start Tracking</button>
+          </div>
+        ) : (
+          <div id="greet-input">
+            <div>{taskName} is being tracked...</div>
+            <button onClick={stopTracking}>Stop Tracking</button>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
